15ee3791a73c768f9bce7bdf562c02d87ea123b4
JZA <jza@gultab.org>
2018-08-20 12:41:40 -0500

    add new languages

--- js/landing/translate.js
+++ js/landing/translate.js
@@ -3,6 +3,55 @@
 })();
 $(document).ready(function () {
 
+    $("#pt-br").on("click", function (e) {
+
+        (function () {
+            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "pt-br", asyncLangLoad: false});
+        })();
+    });
+
+    $("#italian").on("click", function (e) {
+
+        (function () {
+            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "it", asyncLangLoad: false});
+        })();
+    });
+
+    $("#french").on("click", function (e) {
+
+        (function () {
+            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "fr", asyncLangLoad: false});
+        })();
+    });
+
+    $("#german").on("click", function (e) {
+
+        (function () {
+            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "de", asyncLangLoad: false});
+        })();
+    });
+
+    $("#japanese").on("click", function (e) {
+
+        (function () {
+            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "ja", asyncLangLoad: false});
+        })();
+    });
+
+    $("#korean").on("click", function (e) {
+
+        (function () {
+            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "kr", asyncLangLoad: false});
+        })();
+    });
+
+    $("#chineese").on("click", function (e) {
+
+        (function () {
+            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "zh", asyncLangLoad: false});
+        })();
+    });
+
     $("#spanish").on("click", function (e) {
 
         (function () {
