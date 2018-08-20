5bf7386070d9e73b11562fcabdc94a725066238b
Tavo <tavo_x99@hotmail.com>
2018-08-19 22:19:10 -0500

    refactoring js code

---
+++ js/landing/popup-video.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.popup-with-zoom-anim').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in'
+    });
+
+});
