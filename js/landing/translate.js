c3e580d3b346833a3bbba6e32c55ad4235d081e6
Gartox <garciatjm@gmail.com>
2018-08-20 20:46:28 -0500

    clean code for change the language

--- js/landing/translate.js
+++ js/landing/translate.js
@@ -1,69 +1,16 @@
-(function () {
-    $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en'});
-})();
-$(document).ready(function () {
 
-    $("#pt-br").on("click", function (e) {
 
-        (function () {
-            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "pt-br", asyncLangLoad: false});
-        })();
+function selectLanguage( language ) {
+   $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: language, asyncLangLoad: false});
+}
+
+$(document).ready(function ( ) {
+
+    selectLanguage( 'en' );
+
+    $(".lang-selector").on("click", function (e) {
+        var language =  e.target.id;
+        selectLanguage( language );
     });
-
-    $("#italian").on("click", function (e) {
-
-        (function () {
-            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "it", asyncLangLoad: false});
-        })();
-    });
-
-    $("#french").on("click", function (e) {
-
-        (function () {
-            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "fr", asyncLangLoad: false});
-        })();
-    });
-
-    $("#german").on("click", function (e) {
-
-        (function () {
-            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "de", asyncLangLoad: false});
-        })();
-    });
-
-    $("#japanese").on("click", function (e) {
-
-        (function () {
-            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "ja", asyncLangLoad: false});
-        })();
-    });
-
-    $("#korean").on("click", function (e) {
-
-        (function () {
-            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "kr", asyncLangLoad: false});
-        })();
-    });
-
-    $("#chineese").on("click", function (e) {
-
-        (function () {
-            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "zh", asyncLangLoad: false});
-        })();
-    });
-
-    $("#spanish").on("click", function (e) {
-
-        (function () {
-            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'en', forceLang: "es", asyncLangLoad: false});
-        })();
-    });
-
-    $("#english").on("click", function (e) {
-
-        (function () {
-            $("[data-translate]").jqTranslate('i18n/index', {defaultLang: 'es', forceLang: "en", asyncLangLoad: false});
-        })();
-    })
 
 });
